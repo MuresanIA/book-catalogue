c296e4e07d1ecaf25b8ff51d570d9a8115491f0f
MuresanIA <muresan.alexandru933@gmail.com>
2020-10-18 21:53:15 +0300

    repository

---
+++ src/main/java/com/mia/bookstore/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package com.mia.bookstore.repository;
+
+import com.mia.bookstore.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+}
