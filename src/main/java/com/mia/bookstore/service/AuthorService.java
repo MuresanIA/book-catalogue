9f2c3b3b02085c5f0a688f975ca756eb81cf8e8d
MuresanIA <muresan.alexandru933@gmail.com>
2020-10-20 11:08:29 +0300

    adding optional to service classes

--- src/main/java/com/mia/bookstore/service/AuthorService.java
+++ src/main/java/com/mia/bookstore/service/AuthorService.java
@@ -5,6 +5,7 @@ import com.mia.bookstore.repository.AuthorRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service("AuthorService")
 public class AuthorService {
@@ -15,11 +16,25 @@ public class AuthorService {
         this.authorRepository = authorRepository;
     }
 
-    public List<Author> findAllAuthors(){
-        return authorRepository.findAll();
+    public List<Author> findAllAuthors() {
+
+        Optional<List<Author>> author = Optional.of(authorRepository.findAll());
+        return author.orElse(null);
     }
 
-    public Author findById(Integer id){
-        return authorRepository.findByAuthorId(id);
+    public Author findById(Integer id) {
+
+        Optional<Author> author = Optional.ofNullable(authorRepository.findByAuthorId(id));
+
+        return author.orElse(null);
+
+    }
+
+    public void saveAuthor(Author author) {
+        authorRepository.save(author);
+    }
+
+    public void deleteAuthor(Integer id) {
+        authorRepository.deleteById(id);
     }
 }
