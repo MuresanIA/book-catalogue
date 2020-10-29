cf7c764bdadc7b2bcd0c3feb9ebeed8433e8fd58
MuresanIA <muresan.alexandru933@gmail.com>
2020-10-29 12:42:06 +0200

    basic thymeleaf configuration #6

--- src/main/java/com/mia/bookstore/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.mia.bookstore.repository;
-
-import com.mia.bookstore.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
