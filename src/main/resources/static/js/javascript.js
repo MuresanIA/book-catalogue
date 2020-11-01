d8cfabad6e38831cf53381356cd3db0fbcf12136
MuresanIA <muresan.alexandru933@gmail.com>
2020-11-01 12:39:31 +0200

    maxlength to createauthor.html

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -15,3 +15,17 @@ function validateFormCreateAuthor() {
     }
     return true;
 }
+
+const text = document.querySelector("text");
+
+text.addEventListener("input", event => {
+    const target = event.currentTarget;
+    const maxLength = target.getAttribute("maxlength");
+    const currentLength = target.value.length;
+
+    if (currentLength >= maxLength) {
+        return console.log("You have reached the maximum number of characters.");
+    }
+
+    console.log(`${maxLength - currentLength} chars left`);
+});
