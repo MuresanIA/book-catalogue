8e7fb399ea384a03b414c6bcfca9f34e34b278b1
MuresanIA <muresan.alexandru933@gmail.com>
2020-11-10 13:35:36 +0200

    controller thymeleaf work #2

--- src/main/java/com/mia/bookstore/controller/thymeleaf/BooksController.java
+++ src/main/java/com/mia/bookstore/controller/thymeleaf/BooksController.java
@@ -1,9 +1,12 @@
 package com.mia.bookstore.controller.thymeleaf;
 
+import com.mia.bookstore.model.Books;
 import com.mia.bookstore.service.BooksService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -20,4 +23,29 @@ public class BooksController {
 
         return "books/books";
     }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/createbooks")
+    public String createBooks(Model model) {
+        model.addAttribute("books", new Books());
+        return "books/createbooks";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = ("/createbooks"))
+    public String createBooks(@ModelAttribute Books books) {
+        booksService.save(books);
+        return "redirect:/books";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = ("/editbooks/{id}"))
+    public String editBooks(Model model, @PathVariable Integer id) {
+        Books books = booksService.findByBookById(id);
+        model.addAttribute("books", books);
+        return "books/editbooks";
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = ("/editbooks/{id}"))
+    public String editBooks(@ModelAttribute Books books, @PathVariable Integer id){
+
+    }
+
 }
