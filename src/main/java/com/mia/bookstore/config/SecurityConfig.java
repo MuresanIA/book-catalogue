c86d363cd6afa201e71927e06e84d83a3f405be8
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-10 10:41:15 +0200

    test commit

--- src/main/java/com/mia/bookstore/config/SecurityConfig.java
+++ src/main/java/com/mia/bookstore/config/SecurityConfig.java
@@ -3,7 +3,7 @@ package com.mia.bookstore.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+//Test
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
