9aa4e54471cdb33b232325676fa7457854a31e9b
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-11 11:52:47 +0200

    Implementing Spring Security #12

---
+++ src/main/java/com/mia/bookstore/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.mia.bookstore.repository;
+
+import com.mia.bookstore.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+}
