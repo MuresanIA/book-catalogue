07aa213afdf32ed8ab26daa054a8a7de5604e0a1
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-11 11:53:22 +0200

    Implementing Spring Security #13

---
+++ src/main/java/com/mia/bookstore/repository/PrivilegeRepository.java
@@ -0,0 +1,7 @@
+package com.mia.bookstore.repository;
+
+import com.mia.bookstore.model.Privilege;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PrivilegeRepository extends JpaRepository<Privilege, Integer> {
+}
