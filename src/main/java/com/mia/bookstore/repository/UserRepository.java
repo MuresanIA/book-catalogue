7c1a335caf1167959ce70c7d99838740aa05b5b0
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-14 10:29:15 +0200

    Implementing Spring Security #25

--- src/main/java/com/mia/bookstore/repository/UserRepository.java
+++ src/main/java/com/mia/bookstore/repository/UserRepository.java
@@ -4,4 +4,9 @@ import com.mia.bookstore.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByEmail(String email);
+
+    @Override
+    void delete(User user);
 }
