54e24e002cde1d0b6ad7a21b207508b95385f912
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-14 10:30:55 +0200

    Implementing Spring Security #26

--- src/main/java/com/mia/bookstore/repository/RoleRepository.java
+++ src/main/java/com/mia/bookstore/repository/RoleRepository.java
@@ -4,4 +4,9 @@ import com.mia.bookstore.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Role findByName(String name);
+
+    @Override
+    void delete(Role role);
 }
