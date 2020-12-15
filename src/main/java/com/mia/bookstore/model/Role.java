0747d317647162248b12754b66b53719e0947a85
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-15 14:31:07 +0200

    Implementing security 5

--- src/main/java/com/mia/bookstore/model/Role.java
+++ src/main/java/com/mia/bookstore/model/Role.java
@@ -1,5 +1,18 @@
 package com.mia.bookstore.model;
 
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "role_id")
+    private Long roleId;
+
+    @Size(min = 3, max = 30)
+    @Column(name = "role_name", length = 30, nullable = false)
+    private String roleName;
 
 }
