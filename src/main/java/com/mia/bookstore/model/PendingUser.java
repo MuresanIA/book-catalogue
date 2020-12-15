173bdbe0cea318e0bc6f3245259afc57727f4f50
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-15 14:46:07 +0200

    Implementing security 8

---
+++ src/main/java/com/mia/bookstore/model/PendingUser.java
@@ -0,0 +1,29 @@
+package com.mia.bookstore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "pending_user")
+public class PendingUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "pending_user_id")
+    private Long pendingUserId;
+
+
+    @Column(name = "activation_code")
+    private String activationCode;
+
+    @Column(name = "expiration_date")
+    private Date expireDate;
+
+}
