8f5c7c6a2f04a1d041465d65cff231198a4d49dc
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-15 14:57:00 +0200

    Implementing security 12

--- src/main/java/com/mia/bookstore/repository/PendingUserRepository.java
+++ src/main/java/com/mia/bookstore/repository/PendingUserRepository.java
@@ -2,6 +2,15 @@ package com.mia.bookstore.repository;
 
 import com.mia.bookstore.model.PendingUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface PendingUserRepository extends JpaRepository<PendingUser, Long> {
+
+    Optional<PendingUser> findByActivationCode(String activationCode);
+
+    @Query("FROM PendingUser p where p.user.username = :username")
+    Optional<PendingUser> findByUsername(@Param("username") String username);
 }
