63ee41b3d3ab48e6dc34df3ce36fd97b0c41736d
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-15 17:53:02 +0200

    Implementing security 20

---
+++ src/main/java/com/mia/bookstore/model/UserList.java
@@ -0,0 +1,14 @@
+package com.mia.bookstore.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserList {
+    private List<User> userList;
+}
