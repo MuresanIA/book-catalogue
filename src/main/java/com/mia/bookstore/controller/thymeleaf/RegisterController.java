109d9b9f47ab46f9e84270019a6f62c31e2e1a39
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-17 13:25:03 +0200

    Implementing security 39

--- src/main/java/com/mia/bookstore/controller/thymeleaf/RegisterController.java
+++ src/main/java/com/mia/bookstore/controller/thymeleaf/RegisterController.java
@@ -3,8 +3,11 @@ package com.mia.bookstore.controller.thymeleaf;
 import com.mia.bookstore.repository.PendingUserRepository;
 import com.mia.bookstore.repository.UserRepository;
 import com.mia.bookstore.service.RandomStringGeneratorService;
+import com.mia.bookstore.service.SendGridEmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class RegisterController {
@@ -18,4 +21,11 @@ public class RegisterController {
     @Autowired
     private RandomStringGeneratorService randomStringGeneratorService;
 
+    @Autowired
+    private SendGridEmailService sendGridEmailService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/register")
+    public String registerUser(){
+        return "security/register";
+    }
 }
