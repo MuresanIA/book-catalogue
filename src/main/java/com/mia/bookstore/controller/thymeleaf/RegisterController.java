9edc9e709b8dbe96143227537f86dabbfd823d31
MuresanIA <muresan.alexandru933@gmail.com>
2020-12-17 12:35:40 +0200

    Implementing security 37

--- src/main/java/com/mia/bookstore/controller/thymeleaf/RegisterController.java
+++ src/main/java/com/mia/bookstore/controller/thymeleaf/RegisterController.java
@@ -1,7 +1,21 @@
 package com.mia.bookstore.controller.thymeleaf;
 
+import com.mia.bookstore.repository.PendingUserRepository;
+import com.mia.bookstore.repository.UserRepository;
+import com.mia.bookstore.service.RandomStringGeneratorService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class RegisterController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PendingUserRepository pendingUserRepository;
+
+    @Autowired
+    private RandomStringGeneratorService randomStringGeneratorService;
+
 }
